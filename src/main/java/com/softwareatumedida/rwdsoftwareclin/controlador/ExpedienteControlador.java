5e27632b47f60ca2a6830a082c93d10393719503
martin <martin@martin-Satellite-L55-B>
2018-11-12 22:09:04 -0600

    guardado de registros

--- src/main/java/com/softwareatumedida/rwdsoftwareclin/controlador/ExpedienteControlador.java
+++ src/main/java/com/softwareatumedida/rwdsoftwareclin/controlador/ExpedienteControlador.java
@@ -3,6 +3,7 @@ package com.softwareatumedida.rwdsoftwareclin.controlador;
 import com.softwareatumedida.rwdsoftwareclin.ejb.ExpedienteFacadeLocal;
 import com.softwareatumedida.rwdsoftwareclin.entity.Expediente;
 import java.io.Serializable;
+import java.util.List;
 import java.util.Objects;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
@@ -18,20 +19,21 @@ public class ExpedienteControlador implements Serializable{
     @EJB
     ExpedienteFacadeLocal expedienteFacadeLocal;
     Expediente expediente;
-    
+    List<Expediente> listaExpediente;
     @PostConstruct
     public void init(){
         expediente = new Expediente();
+        listar();
     }
     
     public void nuevo(){
         try {
             expedienteFacadeLocal.create(expediente);
-            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "", "Se creo el expediente de " + expediente.getPacienteNombre() + " " + expediente.getPacientePApellido() + " " + expediente.getPacienteSApellido());
-            FacesContext.getCurrentInstance().addMessage("loginForm:password", msg);
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "", "Se creo el expediente de " + expediente.getPacienteNombre() + " " + expediente.getPacientePApellido() + " " + expediente.getPacienteSApellido());
+            FacesContext.getCurrentInstance().addMessage("altaPacienteForm:pacienteRadica", msg);
         } catch (Exception e) {
             FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "", "Ocurrio un problema en el almacenamiento del expediente");
-            FacesContext.getCurrentInstance().addMessage("loginForm:password", msg);
+            FacesContext.getCurrentInstance().addMessage("altaPacienteForm:pacienteRadica", msg);
         }
         
     }
@@ -44,6 +46,18 @@ public class ExpedienteControlador implements Serializable{
         expedienteFacadeLocal.remove(expediente);
     }
 
+    public void listar(){
+        listaExpediente = expedienteFacadeLocal.findAll();
+    }
+
+    public List<Expediente> getListaExpediente() {
+        return listaExpediente;
+    }
+
+    public void setListaExpediente(List<Expediente> listaExpediente) {
+        this.listaExpediente = listaExpediente;
+    }
+    
     public Expediente getExpediente() {
         return expediente;
     }
