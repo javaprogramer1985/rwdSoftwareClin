7022e491b59d3cb6b4ee14780593af66ac9aad7f
javaprogramer1985 <javaprogramer1985@gmail.com>
2018-11-13 15:53:45 -0600

    se trara de enviar mensaje

--- src/main/java/com/softwareatumedida/rwdsoftwareclin/controlador/ExpedienteControlador.java
+++ src/main/java/com/softwareatumedida/rwdsoftwareclin/controlador/ExpedienteControlador.java
@@ -11,22 +11,28 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
 
-@ManagedBean
-@RequestScoped
-public class ExpedienteControlador implements Serializable{
-    
+@Named
+@ViewScoped
+public class ExpedienteControlador implements Serializable {
+
     @EJB
     ExpedienteFacadeLocal expedienteFacadeLocal;
     Expediente expediente;
     List<Expediente> listaExpediente;
+    List<Expediente> listaExpedienteSelect;
+    Expediente expedienteSeleccionado;
+
     @PostConstruct
-    public void init(){
+    public void init() {
         expediente = new Expediente();
+        expedienteSeleccionado = new Expediente();
         listar();
     }
-    
-    public void nuevo(){
+
+    public void nuevo() {
         try {
             expedienteFacadeLocal.create(expediente);
             FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "", "Se creo el expediente de " + expediente.getPacienteNombre() + " " + expediente.getPacientePApellido() + " " + expediente.getPacienteSApellido());
@@ -35,18 +41,30 @@ public class ExpedienteControlador implements Serializable{
             FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "", "Ocurrio un problema en el almacenamiento del expediente");
             FacesContext.getCurrentInstance().addMessage("altaPacienteForm:pacienteRadica", msg);
         }
-        
-    }
-    
-    public void modificar(){
-        expedienteFacadeLocal.edit(expediente);
-    }
-    
-    public void eliminar(){
-        expedienteFacadeLocal.remove(expediente);
+
     }
 
-    public void listar(){
+    public void modificar() {
+        expedienteFacadeLocal.edit(expediente);
+    }
+
+    public void eliminar(Expediente expe) {   
+//        try {
+//            expedienteFacadeLocal.remove(expe);
+//            listar();
+//            FacesMessage msg = new FacesMessage("Aviso", "Se elimino el expediente de " + expe.getPacienteNombre() + " " + expe.getPacientePApellido() + " " + expe.getPacienteSApellido());
+//            FacesContext.getCurrentInstance().addMessage("formPaciente:message", msg);
+//        } catch (Exception e) {
+//            FacesMessage msg = new FacesMessage("Aviso", "Ocurrio un problema en el almacenamiento del expediente");           
+//            FacesContext.getCurrentInstance().addMessage(null, msg);
+//           
+//        }
+        FacesMessage msg = new FacesMessage("Aviso", "Ocurrio un problema en el almacenamiento del expediente");           
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
+    }
+
+    public void listar() {
         listaExpediente = expedienteFacadeLocal.findAll();
     }
 
@@ -57,7 +75,7 @@ public class ExpedienteControlador implements Serializable{
     public void setListaExpediente(List<Expediente> listaExpediente) {
         this.listaExpediente = listaExpediente;
     }
-    
+
     public Expediente getExpediente() {
         return expediente;
     }
@@ -66,6 +84,33 @@ public class ExpedienteControlador implements Serializable{
         this.expediente = expediente;
     }
 
-        
+    public Expediente getExpedienteSeleccionado() {
+        return expedienteSeleccionado;
+    }
+
+    public void setExpedienteSeleccionado(Expediente expedienteSeleccionado) {
+        this.expedienteSeleccionado = expedienteSeleccionado;
+    }
+
     
+    public List<Expediente> getCurrentlySelectedExpedientes() {
+        return listaExpedienteSelect;
+    }
+
+    public void onSelect(Expediente expediente, String typeOfSelection, String indexes) {
+        System.out.println("OnSelect:" + expediente + " typeOfSelection: " + typeOfSelection + " indexes: " + indexes);
+        if (null != expediente) {
+            getCurrentlySelectedExpedientes().add(expediente);
+        } else if (null != indexes) {
+            String[] indexArray = indexes.split(",");
+            for (String index : indexArray) {
+                int i = Integer.valueOf(index);
+                Expediente newCar = listaExpediente.get(i);
+                if (!listaExpedienteSelect.contains(newCar)) {
+                    getCurrentlySelectedExpedientes().add(newCar);
+                }
+            }
+        }
+    }
+
 }
